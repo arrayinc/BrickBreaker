5779909a2d0bca3df51a34d7258babb73e07288e
Alex Ricciardi <alex@thearrayschool.com>
2018-04-28 22:00:15 -0600

    Ball Collision detection enable :)

--- js/ball.js
+++ js/ball.js
@@ -1,23 +1,43 @@
 //******* Ball *****
-/* global GOval, gw */
-
-let ballSize = 25;
+/* global GOval, gw, getElementAt */
+let ballPosittionX = 312;
+let ballPosittionY = 686;
+let ballSize = 26;
 let timeStep = 50;
 
 function animatedBall(gw){
-  let ball = GOval( 312.5, 690, ballSize);
+  let ball = GOval(ballPosittionX, ballPosittionY, ballSize);// Ball starting point on the paddle
   ball.setFilled(true);
   gw.add(ball);
 
 
-  let dx = (gw.getWidth() - ballSize) / timeStep;
-  let dy = (gw.getHeight() - ballSize) / timeStep;
+  let dx = ballSize / 2;
+  let dy = ballSize / 1.7;
   let stepCount = 0;
+
+  let ballTopRightConner = 0;
+  let ballTopLeftConner = 0;
+  let ballBottomRightConner = 0;
+  let ballBottomLeftConner = 0;
 
   function step(){
     ball.move(dx, -dy);
     stepCount++;
     if(stepCount === timeStep) clearInterval(timer);
+
+    ballPosittionX = ballPosittionX + dx;
+    ballPosittionY = ballPosittionY - dy;
+
+    ballTopRightConner = gw.getElementAt(ballPosittionX + ballSize, ballPosittionY - ballSize);
+    ballTopLeftConner = gw.getElementAt(ballPosittionX - ballSize, ballPosittionY - ballSize);
+    ballBottomRightConner = gw.getElementAt(ballPosittionX + ballSize, ballPosittionY + ballSize);
+    ballBottomLeftConner = gw.getElementAt(ballPosittionX - ballSize, ballPosittionY + ballSize);
+
+    if(ballTopRightConner !== null || ballTopLeftConner !== null || ballBottomLeftConner !== null|| ballBottomRightConner !== null){
+      dx = -dx;
+      dy = -dy;
+    }
+
   }
-  let timer = setInterval(step, timeStep);
+  let timer = setInterval(step, timeStep);// Calls the step() with a wait of timeStep in ms
 }
