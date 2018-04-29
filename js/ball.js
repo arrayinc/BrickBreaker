2d3d5ab417b3aea13e249fdf1cd308dc364e6e43
Alex Ricciardi <alex@thearrayschool.com>
2018-04-29 00:33:14 -0600

    Implemented the Ball rebound motion when bouncing off an object or a wall, also implemented a game over alert if the paddle misses the ball, by changing the value of the var dy in the ball(), I troubleshooted my code…..:)

--- js/ball.js
+++ js/ball.js
@@ -12,8 +12,7 @@ function animatedBall(gw){
 
 
   let dx = ballSize / 2;
-  let dy = ballSize / 1.7;
-  let stepCount = 0;
+  let dy = ballSize / 2;
 
   let ballTopRightConner = 0;
   let ballTopLeftConner = 0;
@@ -22,22 +21,61 @@ function animatedBall(gw){
 
   function step(){
     ball.move(dx, -dy);
-    stepCount++;
-    //if(stepCount === timeStep) clearInterval(timer);
 
     ballPosittionX = ballPosittionX + dx;
     ballPosittionY = ballPosittionY - dy;
 
+    // Ball collision detection variable
     ballTopRightConner = gw.getElementAt(ballPosittionX + ballSize, ballPosittionY - ballSize);
     ballTopLeftConner = gw.getElementAt(ballPosittionX - ballSize, ballPosittionY - ballSize);
     ballBottomRightConner = gw.getElementAt(ballPosittionX + ballSize, ballPosittionY + ballSize);
     ballBottomLeftConner = gw.getElementAt(ballPosittionX - ballSize, ballPosittionY + ballSize);
 
+    //  Checking for wall
+    // Right wall
+    if (ballPosittionX + ballSize/2 === 650){
+      dx = -dx;
+      return;
+    }
+    // Left Wall, the ball rebounces only
+    if (ballPosittionX - ballSize/2 === 0){
+      dx = -dx;
+      return;
+    }
+    // Top wall
+    if (ballPosittionY - ballSize/2 === 0){
+      dy = -dy;
+      return;
+    }
+    // Under Paddle
+    if (ballPosittionY + ballSize/2 > 705){
+      alert("You lost");// game over
+      clearInterval(timer);
+    }
+    // Checking for objects
     if(ballTopRightConner !== null || ballTopLeftConner !== null || ballBottomLeftConner !== null|| ballBottomRightConner !== null){
+      //If object is present the ball's direction of travel switches, the ball rebounces
       dx = -dx;
       dy = -dy;
+      // Ball Rebounding on a object from the ball top or the ball bottom
+      if (dx > 0 && dy > 0 || dx < 0 && dy < 0 && ballTopRightConner !== null || ballBottomLeftConner !== null){
+        dx = -dx;
+        return;
+      }
+      if (dx > 0 && dy < 0 || dx < 0 && dy > 0 && ballBottomRightConner !== null || ballTopLeftConner !== null){
+        dx = -dx;
+        return;
+      }
+      // Ball Rebounding on a object from the ball left or right side
+      if (dx > 0 && dy > 0 || dx < 0 && dy < 0 && ballTopLeftConner !== null || ballBottomLeftConner !== null){
+        dy = -dy;
+        return;
+      }
+      if (dx > 0 && dy < 0 || dx < 0 && dy > 0 && ballTopRightConner !== null || ballBottomRightConner !== null){
+        dy = -dy;
+        return;
+      }
     }
-
   }
   let timer = setInterval(step, timeStep);// Calls the step() with a wait of timeStep in ms
 }
